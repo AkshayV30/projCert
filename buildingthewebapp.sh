ac837cfab0f45defbcd49b14b4ce880c4ca0b92f
AkshayV30 <51242050+AkshayV30@users.noreply.github.com>
2020-08-19 18:33:11 +0530

    Update buildingthewebapp.sh

--- buildingthewebapp.sh
+++ buildingthewebapp.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-sudo docker build -f Dockerfile -t image .
-sudo docker run -it -d -p 3001:9001
+sudo docker build -f Dockerfile -t imagephp .
+sudo docker run -it -d -p 3001:80 imagephp
